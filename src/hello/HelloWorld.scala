fdb82382b31291e3474ffe62b75c249861125899
TimotheeEmery <t.emery@hotmail.com>
2015-03-16 18:26:45 +0100

    just a test

---
+++ src/hello/HelloWorld.scala
@@ -0,0 +1,28 @@
+package hello
+
+
+
+object HelloWorld {
+  
+  def main(args: Array[String]){
+      println("Hello World!");
+      
+      val x = 30;
+
+      if( x == 10 ){
+         println("Value of X is 10");
+      }else if( x == 20 ){
+         println("Value of X is 20");
+      }else if( x == 30 ){
+         println("Value of X is 30");
+      }else{
+         println("This is else statement");
+      }
+      
+      
+      
+  }
+  
+  
+  
+}
